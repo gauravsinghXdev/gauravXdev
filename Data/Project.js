dcdb728711b15601cec6056f42602d49fc5bca21
Gaurav Singh <gaurav07singh10@gmail.com>
2024-10-01 12:10:07 +0530

    made hero and project section responsive

--- Data/Project.js
+++ Data/Project.js
@@ -3,14 +3,14 @@ module.exports = {
         title: 'College Recommendation System',
         index: '1',
         image: '/collegeRecomSys.png',
-        forPosition: 'card1',
+        forPosition: 'card card1',
         orientation: 'horizontal'
     },
     'E-Court': {
         title: 'E-Court',
         index: '2',
         image: '/ecourt.png',
-        forPosition: 'card2',
+        forPosition: 'card card2',
         orientation: 'horizontal'
     },
     // 'Ersaor': {
@@ -22,28 +22,28 @@ module.exports = {
         title: 'Reseach Paper - ML Model',
         index: '4',
         image: '/Rpaper.jpg',
-        forPosition: 'card4',
+        forPosition: 'card card4',
         orientation: 'horizontal'
     },
     'Xblog': {
         title: 'XBlog',
         index: '5',
         image: '/xblog.png',
-        forPosition: 'card5',
+        forPosition: 'card card5',
         orientation: 'horizontal'
     },
     'Xcleats': {
         title: 'XCleats',
         index: '6',
         image: '/xcleats.png',
-        forPosition: 'card6',
+        forPosition: 'card card6',
         orientation: 'vertical'
     },
     'Xbook': {
         title: 'XBook',
         index: '7',
         image: '/xbook.png',
-        forPosition: 'card7',
+        forPosition: 'card card7',
         orientation: 'horizontal'
     },
 }
